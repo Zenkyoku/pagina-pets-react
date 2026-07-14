817c18a4fc24af59081bc2d6c6b5c2aa46ba64af
DimbelMe <dimbel.me@gmail.com>
2026-07-13 22:34:31 -0400

    mebe?

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const baseUrl = import.meta.env.BASE_URL;
-document.documentElement.style.setProperty("--base-url", baseUrl);
-document.documentElement.style.setProperty("--bg-home", `url(${baseUrl}img/Fondo-pagina.png)`);
-document.documentElement.style.setProperty("--bg-banner", `url(${baseUrl}img/banner.png)`);
-document.documentElement.style.setProperty("--bg-silithus", `url(${baseUrl}img/silithus.png)`);
+const rootStyles = document.documentElement.style;
+rootStyles.setProperty("--bg-home", `url(${baseUrl}img/Fondo-pagina.png)`);
+rootStyles.setProperty("--bg-banner", `url(${baseUrl}img/banner.png)`);
+rootStyles.setProperty("--bg-silithus", `url(${baseUrl}img/silithus.png)`);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
