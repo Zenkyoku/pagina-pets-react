e1eb830f5ce27bd53a636be8b46b911ffe450945
DimbelMe <dimbel.me@gmail.com>
2026-07-13 22:01:31 -0400

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./paginas/Home";
 import Login from "./paginas/Login";
@@ -6,13 +6,14 @@ import Tools from "./paginas/Tools";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter basename={import.meta.env.BASE_URL}>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/tools" element={<Tools />} />
+                <Route path="*" element={<Home />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
